0bb621e8fc54935880d7906968cbbffa7e432a22
HyunWoo <chaosphch@gmail.com>
2018-02-04 05:59:47 +0900

    render new event page : create new event view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,8 @@ class EventsController < ApplicationController
   def index
     render 'index'
   end
+
+  def new
+    render 'new'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   delete '/feeds/:id', to: 'feeds#destroy'
 
   get '/events', to: 'events#index'
+  get '/events/new', to: 'events#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
