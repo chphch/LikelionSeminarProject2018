42bb16531f6b0b3322001ac62f3871f53e758e0f
chae <opera5@snu.ac.kr>
2018-02-05 02:33:37 +0900

    Install devise gem & Make user model
    For squash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203210244) do
+ActiveRecord::Schema.define(version: 20180204172002) do
+
+  create_table "event_comments", force: :cascade do |t|
+    t.integer "event_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_comments_on_event_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.date "date"
@@ -19,11 +27,36 @@ ActiveRecord::Schema.define(version: 20180203210244) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "feed_comments", force: :cascade do |t|
+    t.integer "feed_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feed_id"], name: "index_feed_comments_on_feed_id"
+  end
+
   create_table "feeds", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
