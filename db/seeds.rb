32fcba9ca6885103a2e4fa13b0af7906227899d9
Hmhm lee <lhm1442@gmail.com>
2018-02-06 06:51:20 +0900

    Add FeedLike to DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u = User.create(name: 'Kooja', email: 'a@a.com', password: '123456')
+u1 = User.create(name: 'Kooja', email: 'a@a.com', password: '123456')
+u2 = User.create(name: 'Hyunwoo', email: 'b@b.com', password: '123456')
 
-f1 = Feed.create(title: '첫 번째 Feed 입니다', content: '친구들과 공유하고 싶은 이야기를 남겨봅시다', user_id: u.id)
-f2 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요', user_id: u.id)
+f1 = Feed.create(title: '첫 번째 Feed 입니다', content: '친구들과 공유하고 싶은 이야기를 남겨봅시다', user_id: u1.id)
+f2 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요', user_id: u1.id)
 
-FeedComment.create(content: '댓글도 남길 수 있네요', feed_id: f1.id, user_id: u.id)
+FeedComment.create(content: '댓글도 남길 수 있네요', feed_id: f1.id, user_id: u1.id)
+
+FeedLike.create([
+  {user: u1, feed: f1},
+  {user: u1, feed: f2},
+  {user: u2, feed: f2}
+])
