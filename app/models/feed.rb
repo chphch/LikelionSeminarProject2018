d23245417715a28133c00c007faab7971ff273c9
Hmhm lee <lhm1442@gmail.com>
2018-02-06 10:17:05 +0900

    Add dependent: :destroy to Feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,8 +1,9 @@
 class Feed < ApplicationRecord
   belongs_to :user
-  has_many :feed_comments
+  has_many :feed_comments, dependent: :destroy
 
   # Users : FeedLikes : Feeds M:N Relations
-  has_many :feed_likes
+  # dependent: :destroy means destroying all belonging feed_likes upon destroying a feed
+  has_many :feed_likes, dependent: :destroy
   has_many :liking_users, through: :feed_likes, source: :user
 end
