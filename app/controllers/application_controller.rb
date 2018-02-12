c7cd6ea1c1f237ecae9737a8838dc8240260ba51
jaeyo_on <jyo3on@gmail.com>
2018-02-13 06:37:19 +0900

    04-1. Layout & User List

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end
+
 end
