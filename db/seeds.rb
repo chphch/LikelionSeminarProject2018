425174f1a8b3584ca80c06cf27ea5411ab938017
jaeyo_on <jyo3on@gmail.com>
2018-02-13 07:09:05 +0900

    04-1-1. Fix minor bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,16 @@
 
 u1 = User.create(name: 'Kooja', email: 'a@a.com', password: '123456')
 u2 = User.create(name: 'Hyunwoo', email: 'b@b.com', password: '123456')
+u3 = User.create(name: 'Jaeyoon', email: 'c@c.com', password: '123456')
+u4 = User.create(name: 'Lion', email: 'd@d.com', password: '123456')
 
 f1 = Feed.create(title: '첫 번째 Feed 입니다', content: '친구들과 공유하고 싶은 이야기를 남겨봅시다', user_id: u1.id)
 f2 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요', user_id: u1.id)
+f3 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요', user_id: u2.id)
+f4 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요', user_id: u1.id)
 
 FeedComment.create(content: '댓글도 남길 수 있네요', feed_id: f1.id, user_id: u1.id)
+FeedComment.create(content: '댓글도 남길 수 있네요', feed_id: f2.id, user_id: u3.id)
 
 FeedLike.create([
   {user: u1, feed: f1},
