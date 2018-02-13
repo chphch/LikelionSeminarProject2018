90a35257b21ad8b135efeff4c1f69bffb2a84ec1
jaeyo_on <jyo3on@gmail.com>
2018-02-13 18:27:32 +0900

    04-4. Feed New

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,9 @@ f1 = Feed.create(title: '첫 번째 Feed 입니다', content: '친구들과 공�
 f2 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요', user_id: u1.id)
 f3 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요', user_id: u2.id)
 f4 = Feed.create(title: '이번 겨울은 너무 추운 것 같아요', content: '겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요', user_id: u1.id)
+f4 = Feed.create(title: '안녕하세요', content: '겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요', user_id: u3.id)
+f4 = Feed.create(title: '안녕하세요', content: '겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요', user_id: u3.id)
+f4 = Feed.create(title: '안녕하세요', content: '겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요 겨울엔 태국이나 베트남에서 살고싶네요', user_id: u4.id)
 
 FeedComment.create(content: '댓글도 남길 수 있네요', feed_id: f1.id, user_id: u1.id)
 FeedComment.create(content: '댓글도 남길 수 있네요', feed_id: f2.id, user_id: u3.id)
