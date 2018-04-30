18a90c71ad66ee5a774c456264931e34320ffb90
Hyunwoo Jung <chaosphch@gmail.com>
2018-04-30 21:54:20 +0900

    Create new user when google authenticated user doen't exist

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,12 @@ class User < ApplicationRecord
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data['name'],
-    #        email: data['email'],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
+    unless user
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
     user
   end
 end
