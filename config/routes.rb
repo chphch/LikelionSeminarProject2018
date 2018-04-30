d5da5498d477f3eb14deb49d9accdf25e88fc0c7
Hyunwoo Jung <chaosphch@gmail.com>
2018-04-30 11:20:52 +0900

    Add customized devise controller for user model
    Define omniauth callback routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
   get '/feeds', to: 'feeds#index'
   get '/feeds/new', to: 'feeds#new'
   post '/feeds', to: 'feeds#create'
